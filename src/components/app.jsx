41aedd07d2d6a127601d372c667bb819c3723935
Hasan Niftiyev <hasan.niftiyev@yahoo.com>
2023-04-25 18:15:55 -0700

    Add dynamic state to notes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./header"
 import Note from "./note"
 import Footer from "./footer"
-import notes from "../notes"
-console.log(notes);
+import CreateArea from "./createArea"
+
+
+
 function App() {
+
+    const [notes,setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+            return [...prevNotes,newNote];
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes =>{
+            return prevNotes.filter((noteItem,index) => {
+                return index!==id;
+            });
+        });
+    }
+
     return (<div>
         <Header />
-        {notes.map((noteItem) =>  <Note title = {noteItem.title}
-                content = {noteItem.content} key = {noteItem.id}/>
-         )}
-        
+        <CreateArea onAdd={addNote}/>
+        {notes.map((noteItem,index) => {
+            return <Note key={index} id={index} onDelete={deleteNote} title={noteItem.title} content={noteItem.content} />
+        })}
         <Footer/>
     </div>);
 }
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 
 function Note(props) {
+    function handleDelete() {
+        props.onDelete(props.id);
+    }
     return (<div className="note">
-        <h3>{props.title}</h3><p>{props.content}</p>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+        <button onClick={handleDelete}>DELETE</button>
     </div>);
 }
 
